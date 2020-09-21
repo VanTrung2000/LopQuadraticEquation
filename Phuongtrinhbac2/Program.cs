2903395671a6320bc0dd36dcc4c2d40bc896bf4a
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-21 10:26:44 +0700

    Add project files.

---
+++ Phuongtrinhbac2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Phuongtrinhbac2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadraticequation qu = new Quadraticequation(1, 6, 9);
+            qu.getter();
+           
+            Console.WriteLine("Delta=" + qu.GetDiscriminant());
+            if(qu.GetDiscriminant() > 0)
+            {
+                Console.WriteLine("Phuong trinh co 2 nghiem");
+                Console.WriteLine("X1:" + qu.GetRoot1());
+                Console.WriteLine("X1:" + qu.GetRoot1());
+
+            }
+            else if (qu.GetDiscriminant() == 0)
+            {
+                Console.WriteLine("Phuong trinh co 1 nghiem duy nhat" + qu.GetRoot());
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+
+
+        }
+    }
+}
---
+++ Phuongtrinhbac2/Quadraticequation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phuongtrinhbac2
+{
+    class Quadraticequation
+    {
+        private double a { set; get; }
+        private double b { set; get; }
+        private double c { set; get; }
+
+        public Quadraticequation(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void getter()
+        {
+            Console.WriteLine("a:"+this.a + '\n'+"b:"+this.b+"\n"+"c:"+this.c);
+        }
+        public double GetDiscriminant()
+        {
+            return b * b -( 4 *a *c); 
+        }
+            public void SetDiscriminant()
+            {
+                Console.WriteLine("Delta:", +GetDiscriminant());
+            }
+        public double GetRoot()
+        {
+            return -b / (2.0 * a);
+        }
+        public double GetRoot1()
+        {
+            return (-b + Math.Sqrt(GetDiscriminant())) / (2 * a);
+        }
+        public double GetRoot2()
+        {
+            return (-b - Math.Sqrt(GetDiscriminant())) / (2 * a);
+        }
+
+
+
+    }
+}
